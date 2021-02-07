677b20749563d9e61e3eed811d3386ae991b7edc
Nick <nmcarducci@gmail.com>
2021-02-07 09:58:40 -0500

    ok

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Magnate.mobi,company,one</h1>
+      <h2>generators, splash, model</h2>
+      <img
+        src="https://www.dl.dropboxusercontent.com/s/t5ueazsx7ikk4ia/magnetboi.jpg?dl=0"
+        alt="https://www.dl.dropboxusercontent.com/s/t5ueazsx7ikk4ia/magnetboi.jpg?dl=0"
+      ></img>
+    </div>
+  );
+}
