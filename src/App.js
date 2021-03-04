e350267db7758abe3e5c9361721c616c72b9536a
Nick Carducci <nmcarducci@gmail.com>
2021-03-04 13:20:51 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ export default function App() {
   return (
     <div className="App">
       <h2>
-        <a href="https://magnate.mobi">generators</a>,{" "}
-        splash,{" "}
+        <a href="https://magnate.mobi">generators</a>, splash,{" "}
         <a href="https://magnate.auto">model</a>
       </h2>
       <img
         src="https://www.dl.dropboxusercontent.com/s/t5ueazsx7ikk4ia/magnetboi.jpg?dl=0"
         alt="https://www.dl.dropboxusercontent.com/s/t5ueazsx7ikk4ia/magnetboi.jpg?dl=0"
       ></img>
+      <br />
+      It is just a tarus again, accelerate, mount or just clip whole copper wrap
+      on to ...radius exxacltly in thickness
+      <br />
       <a
         style={{ wordBreak: "break-word" }}
         href="https://www.academia.edu/45132997/Written_for_National_Science_Foundation_Project_Pitch"
