c64bc1e40b4e6864006fa7380e19c0d1d81202a5
Nick Carducci <nmcarducci@gmail.com>
2021-11-18 15:27:44 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,38 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
+    <div
+      style={{
+        position: "relative",
+        minHeight: "100vh",
+        width: "100%",
+        fontFamily: "sans-serif",
+        textAlign: "center",
+        backgroundColor: "rgb(225, 100, 170)"
+      }}
+    >
+      <iframe
+        style={{
+          shapeOutside: "rect()",
+          float: "right",
+          width: "200px",
+          maxWidth: "80%",
+          border: 0
+        }}
+        src="https://www.youtube.com/embed/nKvUf7F4Xn4"
+        title="YouTube video player"
+      />
+      <img
+        style={{
+          shapeOutside: "rect()",
+          float: "left",
+          width: "200px",
+          maxWidth: "80%",
+          border: 0
+        }}
+        src="https://www.dl.dropboxusercontent.com/s/t5ueazsx7ikk4ia/magnetboi.jpg?dl=0"
+        alt="https://www.dl.dropboxusercontent.com/s/t5ueazsx7ikk4ia/magnetboi.jpg?dl=0"
+      ></img>
       What's the problem? combustion of cardbon binds CO2, ice cores count
       carbon. light speed squared cross multiplies and cancels out. the&nbsp;
       <a href="https://micro-theory.com">law of gravity</a>
@@ -10,11 +41,6 @@ export default function App() {
         <a href="https://magnate.mobi">generators</a>, splash,{" "}
         <a href="https://magnate.auto">model</a>
       </h2>
-      <img
-        src="https://www.dl.dropboxusercontent.com/s/t5ueazsx7ikk4ia/magnetboi.jpg?dl=0"
-        alt="https://www.dl.dropboxusercontent.com/s/t5ueazsx7ikk4ia/magnetboi.jpg?dl=0"
-      ></img>
-      <br />
       It is just a tarus again, accelerate, mount or just clip whole iridium
       wrap on to ...radius exxacltly in thickness
       <br />
@@ -25,13 +51,6 @@ export default function App() {
         https://www.academia.edu/45132997/Written_for_National_Science_Foundation_Project_Pitch
       </a>
       <br />
-      <div style={{ position: "relative", width: "300px" }}>
-        <img
-          alt="plagerism, exclusion and monopoly by Government"
-          style={{ width: "100%", height: "auto" }}
-          src="https://www.dl.dropboxusercontent.com/s/vica68gl2199r9j/Screen%20Shot%202021-03-23%20at%201.53.40%20PM.png?dl=0"
-        />
-      </div>
       <br />
       [mass(electrons_compositi)xlightspeedScales; light is the loss of
       electrons but most are exchanged as they move through the air. the point
@@ -42,6 +61,18 @@ export default function App() {
         the Protons themselves
       </a>
       ?]
+      <img
+        alt="plagerism, exclusion and monopoly by Government"
+        style={{
+          height: "auto",
+          shapeOutside: "rect()",
+          float: "right",
+          width: "400px",
+          maxWidth: "80%",
+          border: 0
+        }}
+        src="https://www.dl.dropboxusercontent.com/s/vica68gl2199r9j/Screen%20Shot%202021-03-23%20at%201.53.40%20PM.png?dl=0"
+      />
     </div>
   );
 }
